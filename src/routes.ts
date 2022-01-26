334e49c89ae9c17282c3dd70e4d874edc349f311
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2022-01-25 22:35:03 -0300

    rota para cadastro de entregador

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { Router } from 'express';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
@@ -6,9 +7,12 @@ const routes = Router();
 
 const createClientCOntroller = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post('/client', createClientCOntroller.handle);
 
 routes.post('/authenticate', authenticateClientController.handle);
+
+routes.post('/deliveryman', createDeliverymanController.handle);
 
 export { routes };
