c66e2866fd76d35d5535aa3b48abd74f512f68f6
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2022-01-25 22:21:50 -0300

    criando autenticação do cliente e tratativa de erro inicial

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
+import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { Router } from 'express';
 import { CreateClientController } from './modules/clients/useCases/createClient/CreateClientController';
 
 const routes = Router();
 
 const createClientCOntroller = new CreateClientController();
+const authenticateClientController = new AuthenticateClientController();
 
-routes.post('/client/', createClientCOntroller.handle);
+routes.post('/client', createClientCOntroller.handle);
+
+routes.post('/authenticate', authenticateClientController.handle);
 
 export { routes };
