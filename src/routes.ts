3978081eed765c3a60b50f4780568025ba0f31c9
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2022-01-25 22:58:39 -0300

    cadastro de entregas

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
@@ -8,15 +9,22 @@ const routes = Router();
 
 const createClientCOntroller = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
-const authenticateDeliverymanController = new AuthenticateDeliverymanController();
+const authenticateDeliverymanController =
+  new AuthenticateDeliverymanController();
 const createDeliverymanController = new CreateDeliverymanController();
+const deliveryController = new CreateDeliveryController();
 
 routes.post('/client', createClientCOntroller.handle);
 
 routes.post('/client/authenticate', authenticateClientController.handle);
 
-routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
+routes.post(
+  '/deliveryman/authenticate',
+  authenticateDeliverymanController.handle
+);
 
 routes.post('/deliveryman', createDeliverymanController.handle);
+
+routes.post('/delivery', deliveryController.handle);
 
 export { routes };
